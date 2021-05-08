702ca776dfdf2075bb3312170a1f5da8cf784a96
idaxe <denny.lang@hs-furtwangen.de>
2021-05-09 00:06:48 +0200

    fixed problems

--- Kapitel_2/Aufgabe2.3/Aufgabe2.3_2-3/script.js
+++ Kapitel_2/Aufgabe2.3/Aufgabe2.3_2-3/script.js
@@ -1,20 +1,120 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let nextbutton = document.getElementById("naechsterTeil");
-nextbutton.addEventListener("click", movePageforeward);
-let pervbutton = document.getElementById("vorherigerTeil");
-pervbutton.addEventListener("click", movePagebackward);
-function movePageforeward() {
-    console.log(window.location.pathname);
-    switch (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1)) {
-        case "index.html":
-            window.open("Rumpf.html", "_self");
-            break;
-        case "Rumpf.html":
-            window.open("Boden.html", "_self");
+var Aufg2u3;
+(function (Aufg2u3) {
+    let current = 1;
+    let loaded = ["", "", ""];
+    Aufg2u3.flaschelhaelse = [];
+    Aufg2u3.flaschenwaende = [];
+    Aufg2u3.flaschenboeden = [];
+    Aufg2u3.ausgewaehlt = { oben: undefined, mitte: undefined, unten: undefined };
+    let nextbutton = document.getElementById("naechsterTeil");
+    nextbutton.addEventListener("click", movePageforeward);
+    let pervbutton = document.getElementById("vorherigerTeil");
+    pervbutton.addEventListener("click", movePagebackward);
+    let htmlImgs = [];
+    window.addEventListener("load", windowLoaded);
+    function movePageforeward() {
+        let flaeche = document.getElementById("flaeche");
+        switch (current) {
+            case 1:
+                document.getElementById("flaeche").innerHTML = "";
+                htmlImgs = [];
+                Aufg2u3.mittlereTeile.forEach(bilder => {
+                    let img = document.createElement("img");
+                    img.src = bilder.quelle;
+                    htmlImgs.push(img);
+                    flaeche.appendChild(img);
+                    img.addEventListener("click", function () {
+                        selectImage(img, bilder);
+                    });
+                });
+                current++;
+                break;
+            case 2:
+                document.getElementById("flaeche").innerHTML = "";
+                htmlImgs = [];
+                Aufg2u3.untereTeile.forEach(bilder => {
+                    let img = document.createElement("img");
+                    img.src = bilder.quelle;
+                    htmlImgs.push(img);
+                    flaeche.appendChild(img);
+                    img.addEventListener("click", function () {
+                        selectImage(img, bilder);
+                    });
+                });
+                current++;
+                break;
+            default:
+                console.log("Reached end");
+        }
     }
-}
-function movePagebackward() {
-    console.log(window.location.pathname);
-}
+    function movePagebackward() {
+        let flaeche = document.getElementById("flaeche");
+        switch (current) {
+            case 2:
+                document.getElementById("flaeche").innerHTML = "";
+                htmlImgs = [];
+                Aufg2u3.obereTeile.forEach(bilder => {
+                    let img = document.createElement("img");
+                    img.src = bilder.quelle;
+                    htmlImgs.push(img);
+                    flaeche.appendChild(img);
+                    img.addEventListener("click", function () {
+                        selectImage(img, bilder);
+                    });
+                });
+                current--;
+                break;
+            case 3:
+                document.getElementById("flaeche").innerHTML = "";
+                htmlImgs = [];
+                Aufg2u3.mittlereTeile.forEach(bilder => {
+                    let img = document.createElement("img");
+                    img.src = bilder.quelle;
+                    htmlImgs.push(img);
+                    flaeche.appendChild(img);
+                    img.addEventListener("click", function () {
+                        selectImage(img, bilder);
+                    });
+                });
+                current--;
+                break;
+            default:
+                console.log("Reached end");
+        }
+    }
+    function windowLoaded() {
+        let flaeche = document.getElementById("flaeche");
+        Aufg2u3.obereTeile.forEach(bilder => {
+            let img = document.createElement("img");
+            img.src = bilder.quelle;
+            htmlImgs.push(img);
+            flaeche.appendChild(img);
+            img.addEventListener("click", function () {
+                selectImage(img, bilder);
+            });
+        });
+    }
+    function selectImage(img, bilder) {
+        if (bilder.art == 0) {
+            Aufg2u3.ausgewaehlt.oben = bilder;
+            loaded[0] = bilder.quelle;
+        }
+        else if (bilder.art == 1) {
+            Aufg2u3.ausgewaehlt.mitte = bilder;
+            loaded[1] = bilder.quelle;
+        }
+        else if (bilder.art == 2) {
+            Aufg2u3.ausgewaehlt.unten = bilder;
+            loaded[2] = bilder.quelle;
+        }
+        img.className += "selected";
+        htmlImgs.forEach(pic => {
+            if (pic != img) {
+                pic.classList.remove("selected");
+            }
+        });
+        console.log(loaded);
+    }
+})(Aufg2u3 || (Aufg2u3 = {}));
 //# sourceMappingURL=script.js.map
--- Kapitel_2/Aufgabe2.3/Aufgabe2.3_2-3/script.ts
+++ Kapitel_2/Aufgabe2.3/Aufgabe2.3_2-3/script.ts
@@ -1,3 +1,4 @@
+namespace Aufg2u3 {
 export interface Flaschenteil {
     oben: Bilder;
     mitte: Bilder;
@@ -6,27 +7,135 @@ export interface Flaschenteil {
 export interface Bilder {
     quelle: string;
     art: number;           //für oben/mitte/unten
-    gravour: string;       //Ist die Flasche graviert
-    farbe: string;         //Welche farbe hat die Flasche
+//    gravour: string;       //Ist die Flasche graviert
+//    farbe: string;         //Welche farbe hat die Flasche
 }
+let current: number = 1;
+let loaded: string[] = ["", "", ""];
 
+export let flaschelhaelse: Bilder[] = [];
+export let flaschenwaende: Bilder[] = [];
+export let flaschenboeden: Bilder[] = [];
+export let ausgewaehlt: Flaschenteil = { oben: undefined, mitte: undefined, unten: undefined };
 let nextbutton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("naechsterTeil");
 nextbutton.addEventListener("click", movePageforeward);
 
 let pervbutton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("vorherigerTeil");
 pervbutton.addEventListener("click", movePagebackward);
 
+let htmlImgs: HTMLImageElement[] = [];
+window.addEventListener("load", windowLoaded);
+
 function movePageforeward (): void {
-    console.log(window.location.pathname);
-    switch (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1)) {
-        case "index.html":
-            window.open("Rumpf.html", "_self");
+    let flaeche: HTMLDivElement = <HTMLDivElement>document.getElementById("flaeche");
+    switch (current) {
+        case 1:
+            document.getElementById("flaeche").innerHTML = "";
+            htmlImgs = [];
+
+            mittlereTeile.forEach (bilder => {
+            let img: HTMLImageElement = document.createElement("img");
+            img.src = bilder.quelle;
+            
+            htmlImgs.push(img);
+            flaeche.appendChild(img);
+            img.addEventListener("click", function (): void {
+                selectImage(img, bilder);
+            });
+        });
+            current ++;
             break;
-        case "Rumpf.html":
-            window.open("Boden.html", "_self");
+        case 2:
+            document.getElementById("flaeche").innerHTML = "";
+            htmlImgs = [];
+
+            untereTeile.forEach (bilder => {
+            let img: HTMLImageElement = document.createElement("img");
+            img.src = bilder.quelle;
+            
+            htmlImgs.push(img);
+            flaeche.appendChild(img);
+            img.addEventListener("click", function (): void {
+                selectImage(img, bilder);
+            });
+        });
+            current ++;
+            break;
+        default:
+            console.log("Reached end");
     }
 }
 
 function movePagebackward (): void {
-    console.log(window.location.pathname);
+    let flaeche: HTMLDivElement = <HTMLDivElement>document.getElementById("flaeche");
+    switch (current) {
+        case 2:
+            document.getElementById("flaeche").innerHTML = "";
+            htmlImgs = [];
+
+            obereTeile.forEach (bilder => {
+            let img: HTMLImageElement = document.createElement("img");
+            img.src = bilder.quelle;
+            
+            htmlImgs.push(img);
+            flaeche.appendChild(img);
+            img.addEventListener("click", function (): void {
+                selectImage(img, bilder);
+            });
+        });
+            current --;
+            break;
+        case 3:
+            document.getElementById("flaeche").innerHTML = "";
+            htmlImgs = [];
+
+            mittlereTeile.forEach (bilder => {
+            let img: HTMLImageElement = document.createElement("img");
+            img.src = bilder.quelle;
+            
+            htmlImgs.push(img);
+            flaeche.appendChild(img);
+            img.addEventListener("click", function (): void {
+                selectImage(img, bilder);
+            });
+        });
+            current --;
+            break;
+        default:
+            console.log("Reached end");
+    }
+}
+
+function windowLoaded (): void {
+    let flaeche: HTMLDivElement = <HTMLDivElement>document.getElementById("flaeche");
+    obereTeile.forEach (bilder => {
+                let img: HTMLImageElement = document.createElement("img");
+                img.src = bilder.quelle;
+                htmlImgs.push(img);
+                flaeche.appendChild(img);
+                img.addEventListener("click", function (): void {
+                    selectImage(img, bilder);
+                });
+            });
+}
+
+function selectImage(img: HTMLImageElement, bilder: Bilder): void {
+    if (bilder.art == 0) {
+        ausgewaehlt.oben = bilder;
+        loaded[0] = bilder.quelle;
+    } else if (bilder.art == 1) {
+        ausgewaehlt.mitte = bilder;
+        loaded[1] = bilder.quelle;
+    } else if (bilder.art == 2) {
+        ausgewaehlt.unten = bilder;
+        loaded[2] = bilder.quelle;
+    }
+    img.className += "selected";
+    htmlImgs.forEach(pic => {
+        if (pic != img) {
+            pic.classList.remove("selected");
+        }
+    });
+    console.log(loaded);
+}
 }
