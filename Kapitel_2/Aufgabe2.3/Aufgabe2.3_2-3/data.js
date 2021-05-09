dbc243e79f743f88180b197479adc3bf1c39fc30
idaxe <denny.lang@hs-furtwangen.de>
2021-05-09 04:53:56 +0200

    fixing

--- Kapitel_2/Aufgabe2.3/Aufgabe2.3_2-3/data.js
+++ Kapitel_2/Aufgabe2.3/Aufgabe2.3_2-3/data.js
@@ -5,23 +5,23 @@ var Aufg2u3;
     Aufg2u3.mittlereTeile = [];
     Aufg2u3.untereTeile = [];
     //erzeugung der Flaschenhaelse
-    let flaschenkopf1 = { quelle: "assets/top1.PNG", art: 0 };
-    let flaschenkopf2 = { quelle: "assets/top2.PNG", art: 0 };
-    let flaschenkopf3 = { quelle: "assets/top3.PNG", art: 0 };
+    let flaschenkopf1 = { quelle: "assets/top1.png", art: 0 };
+    let flaschenkopf2 = { quelle: "assets/top2.png", art: 0 };
+    let flaschenkopf3 = { quelle: "assets/top3.png", art: 0 };
     Aufg2u3.obereTeile.push(flaschenkopf1);
     Aufg2u3.obereTeile.push(flaschenkopf2);
     Aufg2u3.obereTeile.push(flaschenkopf3);
     //erzeugung der Flaschenkoerper
-    let flaschenkorpus1 = { quelle: "assets/mid1.PNG", art: 1 };
-    let flaschenkorpus2 = { quelle: "assets/mid2.PNG", art: 1 };
-    let flaschenkorpus3 = { quelle: "assets/mid3.PNG", art: 1 };
+    let flaschenkorpus1 = { quelle: "assets/mid1.png", art: 1 };
+    let flaschenkorpus2 = { quelle: "assets/mid2.png", art: 1 };
+    let flaschenkorpus3 = { quelle: "assets/mid3.png", art: 1 };
     Aufg2u3.mittlereTeile.push(flaschenkorpus1);
     Aufg2u3.mittlereTeile.push(flaschenkorpus2);
     Aufg2u3.mittlereTeile.push(flaschenkorpus3);
     //erzeugung der Flaschenboeden
-    let flaschenboden1 = { quelle: "assets/bot1.PNG", art: 2 };
-    let flaschenboden2 = { quelle: "assets/bot2.PNG", art: 2 };
-    let flaschenboden3 = { quelle: "assets/bot3.PNG", art: 2 };
+    let flaschenboden1 = { quelle: "assets/bot1.png", art: 2 };
+    let flaschenboden2 = { quelle: "assets/bot2.png", art: 2 };
+    let flaschenboden3 = { quelle: "assets/bot3.png", art: 2 };
     Aufg2u3.untereTeile.push(flaschenboden1);
     Aufg2u3.untereTeile.push(flaschenboden2);
     Aufg2u3.untereTeile.push(flaschenboden3);
--- Kapitel_2/Aufgabe2.3/Aufgabe2.3_2-3/data.ts
+++ Kapitel_2/Aufgabe2.3/Aufgabe2.3_2-3/data.ts
@@ -3,25 +3,25 @@ namespace Aufg2u3 {
     export let mittlereTeile: Bilder[] = [];
     export let untereTeile: Bilder[] = [];
     //erzeugung der Flaschenhaelse
-    let flaschenkopf1: Bilder = { quelle: "assets/top1.PNG", art: 0};
-    let flaschenkopf2: Bilder = { quelle: "assets/top2.PNG", art: 0};
-    let flaschenkopf3: Bilder = { quelle: "assets/top3.PNG", art: 0};
+    let flaschenkopf1: Bilder = { quelle: "assets/top1.png", art: 0};
+    let flaschenkopf2: Bilder = { quelle: "assets/top2.png", art: 0};
+    let flaschenkopf3: Bilder = { quelle: "assets/top3.png", art: 0};
     obereTeile.push(flaschenkopf1);
     obereTeile.push(flaschenkopf2);
     obereTeile.push(flaschenkopf3);
 
     //erzeugung der Flaschenkoerper
-    let flaschenkorpus1: Bilder = { quelle: "assets/mid1.PNG", art: 1};
-    let flaschenkorpus2: Bilder = { quelle: "assets/mid2.PNG", art: 1};
-    let flaschenkorpus3: Bilder = { quelle: "assets/mid3.PNG", art: 1};
+    let flaschenkorpus1: Bilder = { quelle: "assets/mid1.png", art: 1};
+    let flaschenkorpus2: Bilder = { quelle: "assets/mid2.png", art: 1};
+    let flaschenkorpus3: Bilder = { quelle: "assets/mid3.png", art: 1};
     mittlereTeile.push(flaschenkorpus1);
     mittlereTeile.push(flaschenkorpus2);
     mittlereTeile.push(flaschenkorpus3);
 
     //erzeugung der Flaschenboeden
-    let flaschenboden1: Bilder = { quelle: "assets/bot1.PNG", art: 2};
-    let flaschenboden2: Bilder = { quelle: "assets/bot2.PNG", art: 2};
-    let flaschenboden3: Bilder = { quelle: "assets/bot3.PNG", art: 2};
+    let flaschenboden1: Bilder = { quelle: "assets/bot1.png", art: 2};
+    let flaschenboden2: Bilder = { quelle: "assets/bot2.png", art: 2};
+    let flaschenboden3: Bilder = { quelle: "assets/bot3.png", art: 2};
     untereTeile.push(flaschenboden1);
     untereTeile.push(flaschenboden2);
     untereTeile.push(flaschenboden3);
