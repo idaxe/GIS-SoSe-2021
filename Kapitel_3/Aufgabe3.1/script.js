140300c51396d0e172cc5b27db9a0a77d938c6a8
idaxe <denny.lang@hs-furtwangen.de>
2021-05-30 03:48:47 +0200

    finalised

--- Kapitel_3/Aufgabe3.1/script.js
+++ Kapitel_3/Aufgabe3.1/script.js
@@ -1,14 +1,24 @@
 "use strict";
 var K3_A1;
 (function (K3_A1) {
-    let formData = new FormData(document.forms[0]);
     let formDatenButton = document.getElementById("senden");
     formDatenButton.addEventListener("click", absenden);
     async function absenden() {
-        let url = "https://dennytestapp.herokuapp.com/";
+        let formData = new FormData(document.forms[0]);
+        for (let entry of formData) {
+            console.log(entry);
+            console.log("name:" + entry[0]);
+            console.log("value:" + entry[1]);
+        }
+        let url = "https://dennytestapp.herokuapp.com";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
-        await fetch(url);
+        console.log(url);
+        let response = await fetch(url);
+        let text = await response.text();
+        console.log("Antwort des Servers:" + text);
+        let anzeige = document.getElementById("response");
+        anzeige.innerText = text;
     }
 })(K3_A1 || (K3_A1 = {}));
 //# sourceMappingURL=script.js.map
--- Kapitel_3/Aufgabe3.1/script.ts
+++ Kapitel_3/Aufgabe3.1/script.ts
@@ -1,17 +1,23 @@
 namespace K3_A1 {
-    let formData: FormData = new FormData(document.forms[0]);
+
     let formDatenButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("senden");
     formDatenButton.addEventListener("click", absenden);
 
     async function absenden(): Promise<void> {
-
-        let url: string = "https://dennytestapp.herokuapp.com/";
+        let formData: FormData = new FormData(document.forms[0]);
+        for (let entry of formData) {
+                console.log(entry);
+                console.log("name:" + entry[0]);
+                console.log("value:" + entry[1]);
+            } 
+        let url: string = "https://dennytestapp.herokuapp.com";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
-        //await fetch(url);
+        console.log(url);
         let response: Response = await fetch(url);
         let text: string = await response.text();
-        console.log("Answer:");
-        console.log(text);
+        console.log("Antwort des Servers:" + text);
+        let anzeige: HTMLDivElement = <HTMLDivElement>document.getElementById("response");
+        anzeige.innerText = text;
     }
 }
