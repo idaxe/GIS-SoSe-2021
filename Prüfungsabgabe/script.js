c452dc0e01f2f20eeab937bf3b3d0e4f63d79e96
idaxe <denny.lang@hs-furtwangen.de>
2021-07-11 03:19:13 +0200

    major update

--- Prüfungsabgabe/script.js
+++ Prüfungsabgabe/script.js
@@ -6,30 +6,32 @@ var pAbgabe;
     let registerbttn = document.getElementById("register");
     registerbttn.addEventListener("click", userRegister);
     async function userLogin() {
-        console.log("Daten testen");
+        console.log("Login Daten testen");
         let formData = new FormData(document.forms[0]);
         let url = "https://dennytestapp.herokuapp.com"; //http://localhost:8100 "https://dennytestapp.herokuapp.com"
         let query = new URLSearchParams(formData);
         let response = await fetch(url + "/loginUser?" + query.toString());
         let text = await response.text();
-        /*let registered: boolean = await response.ok;
-        if (registered == true) {
-            window.open("index.html", "_self");
-        } else {
-            console.log("error");
-        }*/
         console.log("test");
         console.log(text);
+        let anzeige = document.getElementById("errorMessage");
         sessionStorage.clear();
         if (text != "Nutzer existiert nicht!") {
-            sessionStorage.setItem("nutzer", JSON.stringify(text));
+            sessionStorage.setItem("nutzer", text);
+            let text2 = "Nutzer existiert" + text;
+            console.log(text2);
+            console.log(JSON.stringify(text2));
+            console.log(sessionStorage.getItem("nutzer"));
+            anzeige.innerHTML = "Nutzer existiert";
         }
-        let anzeige = document.getElementById("errorMessage");
-        //console.log(text);
-        anzeige.innerHTML = "Daten abgesendet!";
-        //document.getElementById("email").innerHTML = "";
+        else {
+            anzeige.innerHTML = text;
+        }
         if (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1) == "Login.html") {
             window.open("index.html", "_self");
+        }
+        else if (text != "Nutzer existiert nicht!") {
+            window.open("Meine Rezepte.html", "_self");
         }
         else {
             console.log("Error");
--- Prüfungsabgabe/script.ts
+++ Prüfungsabgabe/script.ts
@@ -7,33 +7,33 @@ namespace pAbgabe {
     registerbttn.addEventListener("click", userRegister);
 
     async function userLogin(): Promise<void> {
-        console.log("Daten testen");
+        console.log("Login Daten testen");
         let formData: FormData = new FormData(document.forms[0]);
         let url: string = "https://dennytestapp.herokuapp.com"; //http://localhost:8100 "https://dennytestapp.herokuapp.com"
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         let response: Response = await fetch(url + "/loginUser?" + query.toString());
         let text: string = await response.text();
-        /*let registered: boolean = await response.ok;
-        if (registered == true) {
-            window.open("index.html", "_self");
-        } else {
-            console.log("error");
-        }*/
         console.log("test");
         console.log(text);
+        let anzeige: HTMLDivElement = <HTMLDivElement>document.getElementById("errorMessage");
         sessionStorage.clear();
         if (text != "Nutzer existiert nicht!") {
-            sessionStorage.setItem("nutzer", JSON.stringify(text));
+            sessionStorage.setItem("nutzer", text);
+            let text2: string = "Nutzer existiert" + text;
+            console.log(text2);
+            console.log(JSON.stringify(text2));
+            console.log(sessionStorage.getItem("nutzer"));
+            anzeige.innerHTML = "Nutzer existiert";
+        } else {
+            anzeige.innerHTML = text;
         }
-        let anzeige: HTMLDivElement = <HTMLDivElement>document.getElementById("errorMessage");
-        //console.log(text);
-        anzeige.innerHTML = "Daten abgesendet!";
-        //document.getElementById("email").innerHTML = "";
 
         if (window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1) == "Login.html") {
             window.open("index.html", "_self");
 
-        } else {console.log("Error"); }
+        } else if (text != "Nutzer existiert nicht!") {
+            window.open("Meine Rezepte.html", "_self");
+        } else {console.log("Error");  }
         //window.open("index.html", "_self");
     }
 
